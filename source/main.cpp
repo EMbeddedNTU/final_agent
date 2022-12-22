39711cc769116bc7a74d95e606f9beaeee8ccaa8
Max <max@schrodingcat.com>
2022-12-22 19:10:05 +0800

    add notification

--- source/main.cpp
+++ source/main.cpp
@@ -10,12 +10,16 @@
 #include <pch.h>
 
 #define PWM_OUT PA_15
+#define LOCK_OUT PA_4
 #define T 0.01
 
 PwmOut CLK(PWM_OUT);
+DigitalOut LOCK(LOCK_OUT);
+InterruptIn button(BUTTON1);
+GSH::HttpService &http_service = GSH::HttpService::GetInstance();
 static events::EventQueue event_queue(16 * EVENTS_EVENT_SIZE);
 
-const static int DEVICE_ID = 2;
+const static int DEVICE_ID = 1;
 const static char WIFI_SSID[] = "509-2";
 const static char WIFI_KEY[] = "max30201";
 
@@ -28,13 +32,30 @@ void updateLightState(double state) {
   CLK.write(state);
 }
 
-void updateLockState(int state) {}
+void send_notification() {
+  char msg[1024];
+  sprintf(msg,
+          "{\"title\": \"門鈴響了\",\"time\": \"\",\"content\": "
+          "\"Agent%d的門鈴響了\"}",
+          DEVICE_ID);
+
+  SharedPtr<GSH::HttpService::HttpResponse> response = http_service.http_post(
+      "http://192.168.0.101:3000/notification/postNotification", NULL, msg);
+}
+
+void button_pressed() { event_queue.call(send_notification); }
+
+void updateLockState(int state) {
+  if (state == 1) {
+    LOCK = true;
+  } else if (state == 0) {
+    LOCK = false;
+  }
+}
 
 void polling() {
-  GSH::HttpService &http_service = GSH::HttpService::GetInstance();
-
   SharedPtr<GSH::HttpService::HttpResponse> response =
-      http_service.http_get("http://192.168.0.101:3000/agent/2", NULL);
+      http_service.http_get("http://192.168.0.101:3000/agent/1", NULL);
   if (response->body != "") {
     for (int i = 0; i < response->body.length(); i += 2) {
       if (response->body[i] == '0') {
@@ -64,6 +85,8 @@ int main() {
 
   SharedPtr<GSH::HttpService::HttpResponse> response = http_service.http_post(
       "http://192.168.0.101:3000/agent/register", NULL, msg);
+
+  button.fall(&button_pressed);
 
   event_queue.call_every(1s, polling);
   advertising.start();
